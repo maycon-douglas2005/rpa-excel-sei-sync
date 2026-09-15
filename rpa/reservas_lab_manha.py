c07f1f3e95cb1dca67ea69c4c8f592f52ce90578
maycon-douglas2005 <mayconchaves2005@gmail.com>
2026-09-14 21:25:33 -0300

    feat: automatiza inserção dos dados Motivos, Nome do Professor e Sala de laboratório utilizada

---
+++ rpa/reservas_lab_manha.py
@@ -0,0 +1,194 @@
+import pyautogui
+import time
+
+import descobrir_semana
+
+
+"""
+
+Pré-requisitos: 
+
+- Estar com a planilha de reservas aberta no mesmo monitor que o navegador. 
+- Estar com o dia desejado já selecionado no filtro de dia na planilha das reservas 
+- Estar com navegador aberto logado no SEI.
+
+"""
+
+# Recomendações: Estar com  apenas uma janela do navegador aberta.
+
+
+# ACESSANDO OCORRENCIA PATRIMONIO
+pyautogui.hotkey('ctrl', 'shift', 'esc')
+time.sleep(1)
+pyautogui.hotkey('alt', 'n')
+pyautogui.write("https://sei.pentagonoedu.com.br/visaoAdministrativo/patrimonio/ocorrenciaPatrimonioForm.xhtml")
+pyautogui.press('enter')
+time.sleep(1)
+
+
+
+##selecionando 'Tipo Ocorrência'
+pyautogui.press('tab', presses=4,  interval=0.5)
+time.sleep(1)
+pyautogui.press('space')
+time.sleep(1)
+pyautogui.press('down', presses=7, interval=0.1)
+time.sleep(0.5)
+pyautogui.press('enter')
+time.sleep(2)
+
+
+# PEGANDO DADOS DA PLANILHA E COLOCANDO NO FORM DE OCORRENCIA
+
+##acessando planilha de reserva
+pyautogui.press("win")
+time.sleep(2)
+pyautogui.write("Documentos: AGENDAMENTO-MANHÃ-1SEM2026")
+time.sleep(2)
+pyautogui.press("enter")
+time.sleep(2)
+
+
+
+##pegando dados da planilha e colocando no form
+
+###pegando motivo na planilha
+
+pyautogui.hotkey('ctrl', 'l')
+time.sleep(2)
+pyautogui.write("AGENDAMENTO")
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.press('esc')
+time.sleep(2)
+pyautogui.press('down')
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(2)
+
+
+
+
+
+###inserindo motivo no form
+
+pyautogui.hotkey('alt', 'tab')                      
+time.sleep(2)
+pyautogui.click(x=235, y=800)
+pyautogui.press('tab', presses=5, interval=0.7)
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'v')
+
+
+# TESTADO ACIMA 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###abrindo campo nome no form
+time.sleep(2)
+pyautogui.press('tab', presses=2, interval=0.5)
+time.sleep(2)
+pyautogui.press('space')
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'a')
+time.sleep(2)
+pyautogui.press('del')
+time.sleep(2)
+
+
+
+###copiando nome na planilha
+pyautogui.hotkey('alt', 'tab')
+time.sleep(2)
+
+
+
+pyautogui.press('F5')
+time.sleep(2)
+pyautogui.write("Professor")
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(2)
+
+pyautogui.press('down')
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(2)
+
+###inserindo nome no form
+pyautogui.hotkey('alt', 'tab')
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(2)
+
+###buscando prof
+pyautogui.press('tab')
+time.sleep(2)
+pyautogui.press('space')
+time.sleep(2)
+
+
+###selecionando prof
+pyautogui.press('tab', presses=6, interval=0.5)
+time.sleep(2)
+pyautogui.press('space')
+
+
+#DESCOBRIR SEMANA E PEGAR NUMERO DA SALA
+time.sleep(2)
+coluna_semana = descobrir_semana.retornarSemana() + 1
+time.sleep(2)
+##copiando sala
+pyautogui.hotkey('alt', 'tab') #volta pra planilha de reservas
+time.sleep(2)
+pyautogui.press('F5')
+time.sleep(2)
+pyautogui.write("Horario")
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(2)
+
+pyautogui.press('down')
+time.sleep(2)
+pyautogui.press('right', presses=coluna_semana, interval=0.2)
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(2)
+
+pyautogui.hotkey('alt', 'tab')
+time.sleep(2)
+pyautogui.click(x=235, y=800)
+time.sleep(2)
+pyautogui.press('tab', presses=10, interval=0.3)
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(2)
+
+##escrevendo sala no form 
+pyautogui.press('tab', presses=2, interval=0.2)
+time.sleep(2)
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(2)
+pyautogui.press('tab')
+time.sleep(2)
+pyautogui.press('space')
+time.sleep(2)
+
+##selecionando sala no form
+pyautogui.press('tab', presses=7, interval=0.2)
+time.sleep(2)
+pyautogui.press('space')
+time.sleep(2)
---
+++ rpa/teste.py
@@ -0,0 +1,17 @@
+#ARQUIVO USADO PARA TESTAR SCRIPTS ANTES DE INSERIR NO ARQUIVO PRINCIPAL
+"""
+import pyautogui
+import time
+import descobrir_semana
+
+
+time.sleep(3)
+print(pyautogui.position())
+
+
+
+teste = descobrir_semana.retornarSemana() + 1
+
+print(teste)
+
+"""
